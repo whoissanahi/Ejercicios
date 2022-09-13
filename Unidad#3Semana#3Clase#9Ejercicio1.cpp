5d8feb14cd84574944b2e314641221fbff836ad7
whoissanahi <113388215+whoissanahi@users.noreply.github.com>
2022-09-13 11:00:50 -0600

    Add files via upload

---
+++ Unidad#3Semana#3Clase#9Ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numero;
+	do
+	{
+		cout << "Ingrese un numero";
+		cin >> numero;
+	}
+	while (numero <= 100);
+	system 	("PAUSE");
+	return 0;
+}	
+
